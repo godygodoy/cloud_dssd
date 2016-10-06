428f8c85176a9ee1c584e463c205aef9013783a9
unknown <you@example.com>
2016-10-06 17:33:36 -0300

    app de google agregada

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require_once 'google-api-php-client-2.0.3/vendor/autoload.php'; ?>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="google-signin-client_id" content="1001512274294-bfavfu667u0reshvha86gtd4agdr32qu.apps.googleusercontent.com">
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+    </head>
+    <body>
+        <div>TODO write content</div>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        <script>
+            function onSignIn(googleUser) {
+                console.log("hola");
+                var profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail());
+                console.log("hola");
+              }
+         </script>     
+    </body>
+</html>
+
